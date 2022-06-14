da70ec7aa6ba1774e589db33d66dddab8e96832c
kwYuk <kwyuk@newbizstart.com>
2022-06-14 20:23:05 +0900

    example nft hardhat

---
+++ hardhat.config.ts
@@ -0,0 +1,45 @@
+import * as dotenv from "dotenv";
+
+import { HardhatUserConfig, task } from "hardhat/config";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-waffle";
+import "@typechain/hardhat";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+
+dotenv.config();
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.4",
+  defaultNetwork: "goerli",
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/BBnHSrW7D91OG8vSfCW5z_g4n4wI9a1k`,
+      accounts: [
+        `ca4418b205a9cdc7c3237020e50d951cc6ce486a6725f7f2428f96b8ed4d54d7`,
+      ],
+    },
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+};
+
+export default config;
---
+++ scripts/contracts/deployERC1155Contract.ts
@@ -0,0 +1,29 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+import { ethers } from 'hardhat';
+
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  // We get the contract to deploy
+  const ERC1155Contract = await ethers.getContractFactory('ERC1155Contract');
+  const erc1155Contract = await ERC1155Contract.deploy('');
+  await erc1155Contract.deployed();
+
+  console.log('ERC1155Contract deployed to:', erc1155Contract.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
